dfdf6da95f4eac08a82ac4b28790efdfc95764fe
Rizki Afrizal <srizkiafrizal85@gmail.com>
2024-06-27 22:56:29 +0700

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'airi',
-  password: 'admin',
-  port: 5432,
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_DB || 'airi',
+  user: process.env.DB_UNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'admin',
+  port: process.env.DB_PORT || 5432,
 })
 
 module.exports = db;
